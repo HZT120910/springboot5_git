12bddd675aac948cd4fce84c5c1094bd88362369
thomas_glb <737218196_glb@qq.com>
2020-06-09 17:04:09 +0800

    项目经理第一次提交项目springboot5

---
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.atguigu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shkstart
+ * @create 2020-06-09 16:53
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("info1")
+    public String info(){
+        return "hello springboot info";
+    }
+}
