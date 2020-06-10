930f946056bb0d821ed5791a34ac6fd8fedf39f9
thomas_glb <737218196_glb@qq.com>
2020-06-10 11:25:49 +0800

    这是开发人员的第二次提交

--- src/main/java/com/atguigu/controller/HelloController.java
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -19,4 +19,9 @@ public class HelloController {
     public String developor(){
         return "hello develepor";
     }
+
+    @RequestMapping("info3")
+    public String developor01(){
+        return "hello develepor";
+    }
 }
